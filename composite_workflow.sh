4b90d8fd2aeac04fef11fac3a54ebed1e4641b7d
Jussi Tiira <jussi.tiira@helsinki.fi>
2017-11-28 15:02:14 +0200

    workflow logging

--- composite_workflow.sh
+++ composite_workflow.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
+# usage: composite_workflow.sh /log/dir
 cfdir=cf
+logdir=$1
 echo "### Raw conversion."
-raw2cfrad.py -v -o $cfdir ???data/*/*.raw ???data/*/*/*.RAW*
+raw2cfrad.py -v -o $cfdir ???data/*/*.raw ???data/*/*/*.RAW* > $logdir/raw2cfrad.log 2> $logdir/raw2cfrad.err
 echo "### Adding extra variables."
-var2nc.sh "$cfdir/*/cfrad.*.nc"
+mlenv.sh var2nc "$cfdir/*/cfrad.*.nc" > $logdir/var2nc.log 2> $logdir/var2nc.err
 echo "### Checking KUM data for missing KDP."
-recover_kdp.py -v $cfdir/*/cfrad.*Kum*.nc
+recover_kdp.py -v $cfdir/*/cfrad.*Kum*.nc > $logdir/recover_kdp.log 2> $logdir/recover_kdp.err
 echo "### Gridding."
-gridding.sh $cfdir
+gridding.sh $cfdir > $logdir/gridding.log 2> $logdir/gridding.err
+
