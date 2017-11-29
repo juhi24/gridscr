ad895cb936cc62f592061fb13527f8bb3eb7ab42
Jussi Tiira <jussi.tiira@helsinki.fi>
2017-11-29 11:48:58 +0200

    video cropping, add usage

--- png2vid.sh
+++ png2vid.sh
@@ -1,2 +1,18 @@
 #!/bin/bash
-ffmpeg -framerate 10 -pattern_type glob -i "$1" -c:v libx264 $2
+
+usage() {
+	echo "usage: $0 \"INPUT_PATTERN\" output_filename.mkv"
+}
+
+while getopts ":h" opt; do
+	case $opt in
+		h)
+			usage
+			exit 0;;
+		\?)
+			echo "Invalid option: -$OPTARG" >&2
+			exit 1;;
+	esac
+done
+
+ffmpeg -framerate 10 -pattern_type glob -i "$1" -s 1920x1080 -c:v libx264 -preset slow $2
