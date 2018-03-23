810b758875ce54242108b86761330ea283f57a90
Jussi Tiira <jussi.tiira@helsinki.fi>
2018-03-23 13:12:35 +0200

    geotiff logic ready

--- radpy/gtiff.py
+++ radpy/gtiff.py
@@ -3,33 +3,42 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 __metaclass__ = type
 
 import rasterio
-import numpy as np
-import matplotlib.pyplot as plt
 from pyproj import Proj, transform
 from os import path
 from scipy.io import loadmat
 
 
+GRID_FILE = '/usr/local/share/radpy/grid.mat'
+
+
+def grid_transform(shape, gridpath=GRID_FILE, p2=Proj(init='epsg:3879')):
+    p1 = Proj(init='epsg:4326') # WGS 84
+    grid = loadmat(gridpath)
+    xs, ys = transform(p1, p2, grid['lon'], grid['lat'])
+    return rasterio.transform.from_bounds(xs[200,0], ys[-1, 200],
+                                          xs[200,-1], ys[0, 200],
+                                          width=shape[1], height=shape[0])
+
+
+def savetif(r, metadata, tifpath, **kws):
+    trans = grid_transform(shape=r.shape, **kws)
+    with rasterio.open(tifpath, 'w', driver='GTiff', height=r.shape[0],
+                       width=r.shape[1], count=1, dtype=r.dtype,
+                       crs='EPSG:3879', transform=trans) as dst:
+        dst.write(r, 1)
+        dst.update_tags(**metadata)
+
+
+def mat2tif(matpath, tifpath, **kws):
+    """Convert deprecated mat output to geotiff."""
+    data = loadmat(matpath)
+    r = data['R'].astype(rasterio.float32).T
+    meta = {'TIFFTAG_DATETIME': data['time'][0]}
+    savetif(r, meta, tifpath, **kws)
+
+
 if __name__ == '__main__':
-    grid_origin_lat = 60.287500
-    grid_origin_lon = 24.982076
-    crs = rasterio.crs.CRS({"proj": "aeqd", "lat_0": grid_origin_lat, "lon_0": grid_origin_lon, "datum": "WGS84"})
-    p1 = Proj(init='epsg:4326')
-    #p2=Proj(init='epsg:3879')
-    p2 = Proj(crs)
-    xo, yo = transform(p1, p2, grid_origin_lon, grid_origin_lat)
     hdd = '/media/jussitii/04fafa8f-c3ca-48ee-ae7f-046cf576b1ee'
     mat = path.join(hdd, 'composite/20171003/mat/20171003_0231.mat')
     gmat = path.join(hdd, 'grid.mat')
-    data = loadmat(mat)
-    r = data['R'].astype(rasterio.float32).T
-    tstr = data['time'][0]
-    grid = loadmat(gmat)
-    xs, ys = transform(p1, p2, grid['lon'], grid['lat'])
-    trans = rasterio.transform.from_bounds(-50000, 50000,
-                                   50000, -50000,
-                                   width=r.shape[1], height=r.shape[0])
-    with rasterio.open('/tmp/new1.tif', 'w', driver='GTiff', height=r.shape[0],
-                   width=r.shape[1], count=1, dtype=r.dtype,
-                   crs=crs, transform=trans) as dst:
-        dst.write(r, 1)
+    mat2tif(mat, '/tmp/new.tif', gridpath=gmat)
